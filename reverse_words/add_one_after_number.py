21bca9b2359fc37bb88cef798861bf3aeb02ac35
venkat1817 <dvenkataramir@gmail.com>
2023-08-13 20:28:50 +0530

    add new code

--- reverse_words/add_one_after_number.py
+++ reverse_words/add_one_after_number.py
@@ -1,9 +1,9 @@
-#add next number in the list.
-# list=[1,3,5,7,9,11,13,15,17]
-# list1=[]
-# for i in range(1,18):
-#     list1.append(i)
-#     print(list1)
+# add next number in the list.
+list=[1,3,5,7,9,11,13,15,17]
+list1=[]
+for i in range(1,18):
+    list1.append(i)
+    print(list1)
 
 
 # add next number in the list.
